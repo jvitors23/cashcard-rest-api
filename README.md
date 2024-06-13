c9a11357c1229e7ddbffedb4d0cde916e0ab04b2
José Vitor Santos Silva <jvss23031999@gmail.com>
2024-06-13 20:51:57 +0100

    feat: cashcards rest api

---
+++ README.md
@@ -0,0 +1,33 @@
+# CashCard Rest API
+
+Spring boot web application implementing simple CRUD rest API for managing cashcards. 
+
+Project from [Building a REST API with Spring Boot](https://spring.academy/courses/building-a-rest-api-with-spring-boot) spring academy course.
+
+## Running the project
+
+You can run the project by executing:
+
+```
+./gradlew bootRun
+```
+
+The application will be available on [http://localhost:8080/](http://localhost:8080/)
+
+## Running tests
+
+To run tests, use:
+
+```
+./gradlew test
+```
+
+## Technologies
+
+- [Java](https://www.java.com/en/)
+- [Gradle](https://gradle.org/)
+- [Spring Boot](https://spring.io/projects/spring-boot)
+  - [Spring Web](https://docs.spring.io/spring-boot/reference/web/index.html)
+  - [Spring Data](https://spring.io/projects/spring-data)
+  - [Spring Security](https://spring.io/projects/spring-security)
+- [Junit](https://junit.org/junit5/)
---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,79 @@
+package example.cashcard;
+
+
+import example.cashcard.models.CashCard;
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+class CashCardJsonTest {
+
+    @Autowired
+    private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45, "jose"),
+                new CashCard(100L, 1.00, "jose"),
+                new CashCard(101L, 150.00, "jose"));
+    }
+
+    @Test
+    void cashCardSerializationTest() throws IOException {
+        CashCard cashCard = cashCards[0];
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("single.json");
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
+                .isEqualTo(99);
+        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
+                .isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+                {
+                    "id": 99,
+                    "amount": 123.45, 
+                    "owner": "jose"
+                }
+                """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45, "jose"));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
+
+    @Test
+    void cashCardListDeserializationTest() throws IOException {
+        String expected = """
+                [
+                     {"id": 99, "amount": 123.45 , "owner": "jose"},
+                     {"id": 100, "amount": 1.00 , "owner": "jose"},
+                     {"id": 101, "amount": 150.00, "owner": "jose"}
+                                                  
+                ]
+                """;
+        assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
+    }
+}
